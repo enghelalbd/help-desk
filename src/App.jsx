8d51f900e3f80727258261310bd87947bb285821
enghelalbd <informhelal@yahoo.com>
2026-01-02 08:11:13 +0600

    Main Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,28 @@ const App = () => {
     <div>
       <Navbar></Navbar>
       <h2 className="text-red-500">Main Section</h2>
+
+      <div className="grid grid-cols-3">
+        <div
+          className="rounded-md
+ p-4 text-amber-3000 bg-purple-500 m-4 h-[150px]"
+        >
+          <h2 className="font-bold text-[35px]"> Pending </h2>
+          <p className="font-semibold text-[22px]"> 0</p>
+        </div>
+        <div
+          className="rounded-md
+ p-4 text-amber-3000 bg-red-500 m-4 h-[150px]"
+        >
+          Hello from App component
+        </div>
+        <div
+          className="rounded-md
+ p-4 text-amber-3000 bg-purple-500 m-4 h-[150px]"
+        >
+          Hello from App component
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
