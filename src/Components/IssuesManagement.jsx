1a0150b67543c72baa149305ae5a5de727d397f6
enghelalbd <informhelal@yahoo.com>
2026-01-06 20:06:34 +0600

    Card Added

--- src/Components/IssuesManagement.jsx
+++ src/Components/IssuesManagement.jsx
@@ -7,21 +7,51 @@ import CountBox from "./CountBox";
 const IssuesManagement = ({ fetchPromise }) => {
   const [togglestatus, setTogglestatus] = useState("all");
   const initialData = use(fetchPromise);
+  console.log(initialData);
 
   return (
     <div>
       <CountBox></CountBox>
 
-      {/* {initialData.map((issue)=>{return<h2> My issue</h2>;}} */}
-
-      {initialData.map((helal) => {
-        console.log(helal);
-        return <h2 key={helal.id}>{helal.category}</h2>;
-      })}
       <Toggel_btn
         togglestatus={togglestatus}
         setTogglestatus={setTogglestatus}
       />
+      {/* Cards */}
+      <Container>
+        <div className="grid grid-cols-3 gap-3">
+          {initialData.map((issue) => {
+            return (
+              <div
+                className="shadow-md rounded-md cursor-pointer p-4 my-4"
+                key={issue.ticketId}
+              >
+                <div>
+                  {" "}
+                  <img
+                    src={issue.userImg}
+                    alt="issue.requestedBy"
+                    className="h-[30px] w-[50px] rounded-full"
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <h2 className="font-bold text-3xl"> {issue.subject}</h2>
+                  <span>{issue.priority} </span>
+                  <span className="text-slate-500 mx-4">
+                    {" "}
+                    Status: {issue.status}
+                  </span>
+                </div>
+                <br />
+                <h2 className="text-3xl text-slate-700">
+                  {" "}
+                  {issue.description}
+                </h2>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
     </div>
   );
 };
